8b00fd525bac4ce81b80d94286e5f98a9e4a2984
Tyler Weir <tylerw@live.com>
2021-11-21 13:14:51 -0800

    Improved multiplayer camera.

--- GlizzyIO/Assets/Scripts/ThirdPersonMovement.cs
+++ GlizzyIO/Assets/Scripts/ThirdPersonMovement.cs
@@ -1,11 +1,12 @@
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonMovement : MonoBehaviour
-{
+public class ThirdPersonMovement : NetworkBehaviour {
+
     public CharacterController controller;
-    //public Transform cam;
+    public Transform cam;
 
     public float speed = 6f;
 
@@ -13,20 +14,28 @@ public class ThirdPersonMovement : MonoBehaviour
     float turnSmoothVelocity;
 
     // Update is called once per frame
-    void Update()
-    {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f) {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg; 
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle+90, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+    void HandleMovement() {
+            if (isLocalPlayer) {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            if(direction.magnitude >= 0.1f) {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; 
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle+90, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            }
         }
+    }
+
+    public override void OnStartLocalPlayer() {
+        cam = Camera.main.transform;
+    }
+    void Update() {
+        HandleMovement();
     }
 }
