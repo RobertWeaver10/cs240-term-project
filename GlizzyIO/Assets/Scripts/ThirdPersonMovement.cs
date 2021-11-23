f6daddbdbadf1691ea1f34f583caf050e1e8f323
Tyler Weir <tylerw@live.com>
2021-11-23 14:27:11 -0800

    Changed the camera position to a far away view.

--- GlizzyIO/Assets/Scripts/ThirdPersonMovement.cs
+++ GlizzyIO/Assets/Scripts/ThirdPersonMovement.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class ThirdPersonMovement : NetworkBehaviour {
 
     public CharacterController controller;
-    public Transform cam;
 
     public float speed = 6f;
 
@@ -16,24 +15,39 @@ public class ThirdPersonMovement : NetworkBehaviour {
     // Update is called once per frame
 
     void HandleMovement() {
-            if (isLocalPlayer) {
+
+        if (!isLocalPlayer) {return; }
+
+        /*
+        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 110.0f;
+        float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * 10f;
+
+        transform.Rotate(0, moveX, 0);
+
+        controller.Move(new Vector3(0, 0, moveZ));
+        Camera.main.transform.Translate(new Vector3(0, 0, moveZ));
+        */
+        if (isLocalPlayer) {
             float horizontal = Input.GetAxisRaw("Horizontal");
             float vertical = Input.GetAxisRaw("Vertical");
             Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
             if(direction.magnitude >= 0.1f) {
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; 
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg; 
                 float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle+90, ref turnSmoothVelocity, turnSmoothTime);
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
                 Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
                 controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+                Camera.main.transform.Translate(moveDir.normalized * speed * Time.deltaTime);
             }
         }
     }
 
     public override void OnStartLocalPlayer() {
-        cam = Camera.main.transform;
+        //Camera.main.transform.SetParent(transform);
+        Camera.main.transform.localPosition = new Vector3(0, 20, -50);
     }
     void Update() {
         HandleMovement();
