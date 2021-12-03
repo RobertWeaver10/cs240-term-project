2b12a406a867b714ab168fd9c32155aebc4b17d6
Tyler Weir <tylerw@live.com>
2021-12-02 20:34:04 -0800

    Added a floating health bar above players.

--- GlizzyIO/Assets/Scripts/Player.cs
+++ GlizzyIO/Assets/Scripts/Player.cs
@@ -3,18 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : NetworkBehaviour
-{
-    void HandleMovement() {
-        if (isLocalPlayer) {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
-            Vector3 movement = new Vector3(moveHorizontal * 0.25f, 0, moveVertical * 0.25f);
-            transform.position = transform.position + movement;
-        }
+public class Player : NetworkBehaviour {
+
+    int health = 100;
+    public GameObject healthBar;
+
+    public void TakeDamage(int amount) {
+        if (!isServer) return;
+
+        health -= amount;
+        RpcDamage(amount);
+    }    
+
+    [ClientRpc]
+    public void RpcDamage(int amount) {
+        Debug.Log("Took damage:" + amount);
     }
 
     void Update() {
-        HandleMovement();
+        if (health <= 0 ) {
+            // Ask rob how to destroy an object
+        }            
+    }
+
+    void Start() {
+        healthBar.transform.localScale = new Vector3(0.5f, 0.05f, 0.05f);
+        healthBar.GetComponent<Renderer>().material.color = Color.yellow;
     }
 }
