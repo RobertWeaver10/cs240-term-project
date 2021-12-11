9ec69701527cc584d939c7c8cb051dcb87a1cc13
Tyler Weir <tylerw@live.com>
2021-12-11 15:01:48 -0800

    Healthbar dynamically scales and changes color to match health.

--- GlizzyIO/Assets/Scripts/Player.cs
+++ GlizzyIO/Assets/Scripts/Player.cs
@@ -13,7 +13,21 @@ public class Player : NetworkBehaviour {
 
         health -= amount;
         RpcDamage(amount);
+        updateHealthbar();
     }    
+
+    public void heal(int amount) {
+        if (!isServer) return;
+
+        health += amount;
+        RpcHeal(amount);
+        updateHealthbar();
+    }
+
+    [ClientRpc]
+    public void RpcHeal(int amount) {
+        Debug.Log("Healed: " + amount);
+    }
 
     [ClientRpc]
     public void RpcDamage(int amount) {
@@ -21,13 +35,29 @@ public class Player : NetworkBehaviour {
     }
 
     void Update() {
+        // If player dies
         if (health <= 0 ) {
             // Ask rob how to destroy an object
-        }            
+        }      
+    }
+
+
+    void updateHealthbar() {
+        // NOTE: this function should be called anytime the player health is changed.
+
+        // Scale health bar to match health level
+        healthBar.transform.localScale = new Vector3(0.5f * health/100, 0.05f, 0.05f);
+        if(health > 66) {
+            healthBar.GetComponent<Renderer>().material.color = Color.green;
+        } else if (health > 33) {
+            healthBar.GetComponent<Renderer>().material.color = Color.yellow;
+        } else {
+            healthBar.GetComponent<Renderer>().material.color = Color.red;
+        }
+        
     }
 
     void Start() {
-        healthBar.transform.localScale = new Vector3(0.5f, 0.05f, 0.05f);
-        healthBar.GetComponent<Renderer>().material.color = Color.yellow;
+        updateHealthbar();
     }
 }
