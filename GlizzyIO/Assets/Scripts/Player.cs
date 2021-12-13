42d65e9e751c237a93a7351e7268bf521acb9e26
Jacob Ota <jota@pugetsound.edu>
2021-12-12 16:38:49 -0800

    Connected a players death to the death scene

--- GlizzyIO/Assets/Scripts/Player.cs
+++ GlizzyIO/Assets/Scripts/Player.cs
@@ -3,20 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : NetworkBehaviour {
+public class Player : NetworkBehaviour
+{
 
     int health = 100;
     public GameObject healthBar;
 
-    public void TakeDamage(int amount) {
+    public void TakeDamage(int amount)
+    {
         if (!isServer) return;
 
         health -= amount;
         RpcDamage(amount);
         updateHealthbar();
-    }    
+    }
 
-    public void heal(int amount) {
+    public void heal(int amount)
+    {
         if (!isServer) return;
 
         health += amount;
@@ -25,39 +28,51 @@ public class Player : NetworkBehaviour {
     }
 
     [ClientRpc]
-    public void RpcHeal(int amount) {
+    public void RpcHeal(int amount)
+    {
         Debug.Log("Healed: " + amount);
     }
 
     [ClientRpc]
-    public void RpcDamage(int amount) {
+    public void RpcDamage(int amount)
+    {
         Debug.Log("Took damage:" + amount);
     }
 
-    void Update() {
+    void Update()
+    {
         // If player dies
-        if (health <= 0 ) {
+        if (health <= 0)
+        {
             // Ask rob how to destroy an object
-        }      
+            Application.LoadLevel(2);
+        }
     }
 
 
-    void updateHealthbar() {
+    void updateHealthbar()
+    {
         // NOTE: this function should be called anytime the player health is changed.
 
         // Scale health bar to match health level
-        healthBar.transform.localScale = new Vector3(0.5f * health/100, 0.05f, 0.05f);
-        if(health > 66) {
+        healthBar.transform.localScale = new Vector3(0.5f * health / 100, 0.05f, 0.05f);
+        if (health > 66)
+        {
             healthBar.GetComponent<Renderer>().material.color = Color.green;
-        } else if (health > 33) {
+        }
+        else if (health > 33)
+        {
             healthBar.GetComponent<Renderer>().material.color = Color.yellow;
-        } else {
+        }
+        else
+        {
             healthBar.GetComponent<Renderer>().material.color = Color.red;
         }
-        
+
     }
 
-    void Start() {
+    void Start()
+    {
         updateHealthbar();
     }
 }
